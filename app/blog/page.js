393bcdbcd496345ca1eb4dd237bd67230f3c14b2
Elif Keten <elifbeyzaketen@gmail.com>
2026-02-09 20:27:19 +0100

    add initial Blog page

---
+++ app/blog/page.js
@@ -0,0 +1,12 @@
+export default function Blog() {
+  return (
+    <main>
+      <h1>Lorem Ipsum</h1>
+      <p>
+        Ut condimentum ex eu justo volutpat, sit amet viverra leo molestie.
+        Morbi molestie a leo ac hendrerit.
+      </p>
+      <img src="https://picsum.photos/200" alt="Random placeholder" />
+    </main>
+  );
+}
